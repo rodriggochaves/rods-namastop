3763f0ce8c8c2ead7034b6ffd7a28cd2f592a84d
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-12-27 20:38:43 -0200

    add BodyParser to package

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/hello', (req, res) => {
