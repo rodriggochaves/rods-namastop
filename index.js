4d8752a8e9417e11664c37ad5a016ac76ded5a09
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-12-27 15:43:54 -0200

    use node cron

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const PORT = process.env.PORT || 5000
+const cron = require("node-cron");
 const app = require('./app')
+const reminder = require('./src/libs/reminder')
+
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+cron.schedule("* * 15 * * 5", () => {
+  reminder();
+});
