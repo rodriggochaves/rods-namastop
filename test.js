469b1fbf59048e611b99502949c94b02b8304502
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-12-27 11:08:07 -0200

    first test! :tada:

--- test.js
+++ test.js
@@ -1,28 +1,10 @@
-const { spawn } = require('child_process');
-const request = require('request');
-const test = require('tape');
+const request = require('supertest')
+const app = require('./app')
 
-// Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
-
-test('responds to requests', (t) => {
-  t.plan(4);
-
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
-      // stop the server
-      child.kill();
-
-      // No error
-      t.false(error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+describe('rods-namastep', () => {
+  test('/ responds with http 200', async () => {
+    return request(app).get('/').then((response) => {
+      expect(response.statusCode).toBe(200);
     });
   });
 });
