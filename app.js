142b4ec64ac6b5198da16c49ddf0941f69cbf444
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-12-27 11:47:11 -0200

    use index.js

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 module.exports = app
