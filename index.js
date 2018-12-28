7ee6ebbdf645601ed87358a98cb395ecdecaf1a6
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-12-27 23:39:39 -0200

    remove POST /notes from wrong place

--- index.js
+++ index.js
@@ -5,23 +5,9 @@ require('./src/server/models/mongosse').config()
 const PORT = process.env.PORT || 5000
 const cron = require("node-cron");
 const app = require('./app')
-const Note = require('./src/server/models/schema')
 const { remindAllUsers } = require('./src/libs/reminder')
 
-
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-
-app.post('/note', (req, res) => {
-  const username = req.body.user_name
-  const userId = req.body.user_id
-  const text = req.body.text
-
-  const note = new Note({ username, userId, text })
-
-  note.save()
-
-  res.json({ text: "Your gratitude was sent. :blue_heart:" })
-});
 
 cron.schedule("* * 15 * * 5", () => {
   remindAllUsers();
