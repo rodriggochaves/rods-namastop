f2955c5f4c9cbd72baf4d96a40b035e392f27825
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-12-28 08:38:29 -0200

    test client if 3 notes are rendered

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,3 +5,38 @@ import App from './App';
 it('renders without crashing', () => {
   shallow(<App />);
 });
+
+it('renders 3 notes', async () => {
+  global.fetch = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, _) => {
+      resolve({
+        json: () => {
+          return [
+            {
+              "_id": "5c25769ff32d89bd1ccc54ed",
+              "username": "rodrigochaves",
+              "userId": "U48H4EH19",
+              "text": "thanks"
+            },
+            {
+              "_id": "5c2576b000e977bda9830bf6",
+              "username": "rodrigochaves",
+              "userId": "U48H4EH19",
+              "text": "thanks"
+            },
+            {
+              "_id": "5c2576b6271f61be28be904f",
+              "username": "rodrigochaves",
+              "userId": "U48H4EH19",
+              "text": "thanks"
+            },
+          ]
+        }
+      })
+    })
+  })
+
+  const wrapper = await shallow(<App />);
+  await wrapper.instance().callApi()
+  expect(wrapper.find('.comment').length).toEqual(3)
+})
