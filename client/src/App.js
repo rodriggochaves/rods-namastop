5670098de9b8cf96db38ca21110ea55f99214ab6
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-12-28 12:28:41 -0200

    filter notes based in user

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 
 class App extends Component {
   state = {
-    notes: []
+    notes: [],
+    filtered: false
   }
 
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ notes: res }))
-      .catch(err => console.log(err));
+    this.loadNotes()
   }
 
   callApi = async () => {
@@ -23,20 +22,47 @@ class App extends Component {
     return body
   }
 
+  loadNotes = () => {
+    this.callApi()
+      .then(res => this.setState({ 
+        notes: res,
+        filtered: false
+      }))
+      .catch(err => console.log(err));
+  }
+
+  filterByUser = (userId) => {
+    const notes = this.state.notes
+    this.setState({ 
+      notes: notes.filter((note) => note.userId == userId),
+      filtered: true
+    })
+  }
+
+  renderBackButton = () => {
+    if(this.state.filtered) {
+      return <button onClick={() => this.loadNotes()} className="ui button">go back</button>
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="ui container">
         <h1>Namastop</h1>
-          <div className="ui comments">
+        {this.renderBackButton()}
+        <div className="ui comments">
           {this.state.notes.map((note) => {
             return (
               <div key={note._id} className="comment">
                 <div className="avatar">
-                  <img src={note.userAvatar}/>
+                  <img src={note.userAvatar || "https://semantic-ui.com/images/avatar/small/elliot.jpg"}/>
                 </div>
                 <div className="content">
-                  <span className="author">{note.username}</span>&nbsp;said&nbsp;
+                  <span onClick={() => this.filterByUser(note.userId) } className="author">
+                    {note.username}
+                  </span>
+                  <span>&nbsp;said&nbsp;</span>
                   <div className="text">
                     {note.text}
                   </div>
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,37 +6,60 @@ it('renders without crashing', () => {
   shallow(<App />);
 });
 
-it('renders 3 notes', async () => {
-  global.fetch = jest.fn().mockImplementation(() => {
-    return new Promise((resolve, _) => {
-      resolve({
-        json: () => {
-          return [
-            {
-              "_id": "5c25769ff32d89bd1ccc54ed",
-              "username": "rodrigochaves",
-              "userId": "U48H4EH19",
-              "text": "thanks"
-            },
-            {
-              "_id": "5c2576b000e977bda9830bf6",
-              "username": "rodrigochaves",
-              "userId": "U48H4EH19",
-              "text": "thanks"
-            },
-            {
-              "_id": "5c2576b6271f61be28be904f",
-              "username": "rodrigochaves",
-              "userId": "U48H4EH19",
-              "text": "thanks"
-            },
-          ]
-        }
+describe('request to API', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      return new Promise((resolve, _) => {
+        resolve({
+          json: () => {
+            return [
+              {
+                "_id": "5c25769ff32d89bd1ccc54ed",
+                "username": "rodrigochaves",
+                "userId": "U48H4EH19",
+                "text": "thanks"
+              },
+              {
+                "_id": "5c2576b000e977bda9830bf6",
+                "username": "rodrigochaves",
+                "userId": "U48H4EH19",
+                "text": "thanks"
+              },
+              {
+                "_id": "5c2576b6271f61be28be904f",
+                "username": "kilmer",
+                "userId": "U3QPKF69E",
+                "text": "very thanks"
+              },
+            ]
+          }
+        })
       })
     })
   })
 
-  const wrapper = await shallow(<App />);
-  await wrapper.instance().callApi()
-  expect(wrapper.find('.comment').length).toEqual(3)
+  it('renders 3 notes', async () => {
+    const wrapper = await shallow(<App />);
+    await wrapper.instance().callApi()
+    expect(wrapper.find('.comment').length).toEqual(3)
+  })
+
+  it('renders to an user if clicked', async() => {
+    const wrapper = await shallow(<App />);
+    await wrapper.instance().callApi()
+    wrapper.find('.author').at(0).simulate('click')
+    wrapper.update()
+    expect(wrapper.find('.comment').length).toEqual(2)
+  })
+
+  it('can go back after filtered', async() => {
+    const wrapper = await shallow(<App />);
+    await wrapper.instance().callApi()
+    wrapper.find('.author').at(0).simulate('click')
+    wrapper.update()
+    expect(wrapper.find('.comment').length).toEqual(2)
+    wrapper.find('button').simulate('click')
+    await wrapper.instance().callApi()
+    expect(wrapper.find('.comment').length).toEqual(3)
+  })
 })
