ca0b3fe5f95cf3a8b2c4719664574e12c7fd4277
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-12-27 23:38:22 -0200

    list all notes in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {
-    response: '',
-    post: '',
-    responseToPost: '',
+    notes: []
   }
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ notes: res }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  }
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/hello', {
+    const response = await fetch('/notes', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       }
-      // body: JSON.stringify({ post: this.state.post }),
     });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
+    const body = await response.json();
+    return body
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+        <h1>Namastop</h1>
+        {this.state.notes.map((note) => {
+          return (
+            <div key={note._id}>
+              <p>{note.username}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
